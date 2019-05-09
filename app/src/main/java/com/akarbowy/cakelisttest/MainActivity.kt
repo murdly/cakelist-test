cfb86fc20cd8bfdbd242877a7f6d54634b03c2d7
Arek Karbowy <arek.karbowy@gmail.com>
2019-05-09 08:06:38 +0100

    init

---
+++ app/src/main/java/com/akarbowy/cakelisttest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.akarbowy.cakelisttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
